b9b7eb0299c8ccde31dcf4b829d5d74ea4a88a14
drewpgilmore@gmail.com <drewpgilmore@gmail.com>
2020-10-01 23:05:07 -0700

    added constants for mapbox documentation

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,26 +1,76 @@
-// Create an initial map object
-// Set the longitude, latitude, and the starting zoom level
-var myMap = L.map("map").setView([40, -95], 4);
+// mapbox specifics
+const mapboxUrl = "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}";
+const mapboxAttribution = "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>"
 
-// Add a tile layer (the background map image) to our map
-// Use the addTo method to add objects to our map
-var streetMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+var streets = L.tileLayer(mapboxUrl, {
+  id: 'mapbox/streets-v11', 
   tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
-}).addTo(myMap);
-
-var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "dark-v10",
+  maxZoom: 18, 
+  zoomOffset: -1, 
+  attribution: mapboxAttribution,
   accessToken: API_KEY
 });
 
+var light = L.tileLayer(mapboxUrl, {
+  id: 'light-v10', 
+  tileSize: 512,
+  maxZoom: 18, 
+  zoomOffset: -1, 
+  attribution: mapboxAttribution,
+  accessToken: API_KEY
+});
+
+var dark = L.tileLayer(mapboxUrl, {
+  id: 'dark-v10', 
+  tileSize: 512,
+  maxZoom: 18, 
+  zoomOffset: -1, 
+  attribution: mapboxAttribution,
+  accessToken: API_KEY
+});
+
+var map = L.map('map', {
+    center: [0, 0],
+    zoom: 2,
+    layers: [streets, light, dark]
+});
+
+
+
+// Create an initial map object
+// Set the longitude, latitude, and the starting zoom level
+// var myMap = L.map("map").setView([20, 0], 2);
+
+// // Add a tile layer (the background map image) to our map
+// // Use the addTo method to add objects to our map
+// var streetMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+//   tileSize: 512,
+//   maxZoom: 18,
+//   zoomOffset: -1,
+//   id: "mapbox/streets-v11",
+//   accessToken: API_KEY
+// }).addTo(myMap);
+
+// // dark map
+// var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+//   maxZoom: 18,
+//   id: "light-v10",
+//   accessToken: API_KEY
+// });
+
+// // light map
+// var darkMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+//   maxZoom: 18,
+//   id: "dark-v10",
+//   accessToken: API_KEY
+// });
+
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+
+var tremors = L.layerGroup();
 
 d3.json(url, function(response) {
   var earthquakes = response.features;
@@ -49,15 +99,28 @@ d3.json(url, function(response) {
 
     // Add circles to map
     L.circle([lat, lon], {
-      fillOpacity: 0.75,
+      fillOpacity: 0.50,
       color: color,
       fillColor: color,
-      // Adjust radius
       radius: mag * 10000
-    }).bindPopup("<h1>" + title + "</h1>").addTo(myMap);
+    }).bindPopup("<h1>" + title + "</h1>").addTo(tremors);
   
-  }
+  };
 
 
-})
+});
+
+var baseMaps = {
+  "Light": light,
+  "Dark": dark,
+  "Streets": streets,
+};
+
+var overlayMaps = {
+  "Tremors": tremors
+};
+
+
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
 
