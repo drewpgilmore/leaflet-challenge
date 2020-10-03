f90afec6d6b6b1cdde950adb576ef0bac4f5d60c
drewpgilmore@gmail.com <drewpgilmore@gmail.com>
2020-10-02 22:31:58 -0700

    added color variables for map

---
+++ static/js/config.js
@@ -0,0 +1,10 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiZHJld3BnaWxtb3JlIiwiYSI6ImNrZmRhYzEzOTA0c2oycXJ3Nzc4ZGN6ZHQifQ.59iOzOtMtCapARF4Rh6UXA";
+
+var color1 = '#F94144';
+var color2 = '#F3722C';
+var color3 = '#F8961E';
+var color4 = '#F9C74F';
+var color5 = '#90BE6D';
+var color6 = '#43AA8B';
+
