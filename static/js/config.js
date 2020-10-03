585c421bf61f30caaa63a65517891ce9f53d4c5c
drewpgilmore@gmail.com <drewpgilmore@gmail.com>
2020-10-03 01:40:13 -0700

    added more functions and urls for different time periods

--- static/js/config.js
+++ static/js/config.js
@@ -8,3 +8,37 @@ var color4 = '#F9C74F';
 var color5 = '#90BE6D';
 var color6 = '#43AA8B';
 
+var colors = [color1, color2, color3, color4, color5, color6];
+
+const mapboxUrl = "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}";
+const mapboxAttribution = "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>"
+
+const pastHourURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
+const pastDayURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+const pastSevenDaysURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+const pastThirthyDaysURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+
+
+function returnColor(num) {
+    var color = "";
+    
+    if (num > 40) {
+      color = color1;
+    }
+    else if (num > 30) {
+      color = color2;
+    }
+    else if (num > 20) {
+      color = color3;
+    }
+    else if (num > 10) {
+      color = color4;
+    }
+    else if (num > 0) {
+      color = color5;
+    }
+    else {
+      color = color6;
+    }
+    return color
+}
